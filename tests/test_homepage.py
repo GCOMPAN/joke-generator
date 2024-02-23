27778e3c3db845d8b870f81f6ac529725a9cb98c
Gab <gabriel.compan@mines-ales.org>
2024-02-23 15:22:30 +0100

    add test_homepage

---
+++ tests/test_homepage.py
@@ -0,0 +1,12 @@
+import pytest
+from semaphoreflask import app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client    :
+        yield client
+
+def test_homepage_response(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b'HomePage is OK' in response.data
