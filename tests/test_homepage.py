5ce041ff79195340491496e79022820305cc348d
Gab <gabriel.compan@mines-ales.org>
2024-02-23 17:00:44 +0100

    update

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -1,12 +1,12 @@
 import pytest
-from flask import app
+from app import app
 
 @pytest.fixture
 def client():
-    with app.test_client() as client    :
+    with app.test_client() as client:
         yield client
 
 def test_homepage_response(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert b'HomePage is OK' in response.data
+    assert b'Homepage is OK' in response.data
